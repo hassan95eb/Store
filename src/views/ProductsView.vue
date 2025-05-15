253db210f0b72162661f454482d4bc903730ce04
hassan95eb <19h.amini95@gmail.com>
2025-05-15 14:23:20 +0330

    change some db.json

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,6 +1,22 @@
 <template>
   <div>this is products view s</div>
+  <div>{{ products }}</div>
 </template>
+
 <script setup>
-//
+import axios from 'axios'
+import { onMounted, ref } from 'vue'
+const products = ref([])
+onMounted(() => {
+  axios
+    .get('http://localhost:3000/products')
+    .then((res) => {
+      products.value = res.data
+      console.log(res)
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+  console.log('axios run')
+})
 </script>
